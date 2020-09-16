9e3e2be5975887766e1d24ddb3c9ff350b50ab54
Janu <janu.m.sung@gmail.com>
2020-09-16 16:35:03 -0700

    just in case, markers are getting rendered, now to clean up

--- src/components/googleMaps/MapContainer.js
+++ src/components/googleMaps/MapContainer.js
@@ -10,6 +10,7 @@ import Slider from '@material-ui/core/Slider';
 import { makeStyles } from '@material-ui/core/styles';
 import { Button, Input, Divider, message } from 'antd';
 import TextField from '@material-ui/core/TextField';
+import { ContactSupportTwoTone } from '@material-ui/icons';
 
 
 const API_KEY = 'AIzaSyAn899KmcxNfZ5RGR7o0KbaHhhRHJtjFEw'
@@ -63,14 +64,15 @@ function MapsContainer(props) {
     placesService: {},
     geoCoderService: {},
     directionService: {},
+    renderMarkersNow: true
   })
 
-  const { constraints, mapsLoaded, markers, searchResults, map, mapsApi, usersLocation, autoCompleteService, placesService, geoCoderService, directionService } = state;
+  const { constraints, mapsLoaded, markers, searchResults, map, mapsApi, usersLocation, autoCompleteService, placesService, geoCoderService, directionService, renderMarkersNow } = state;
 
 
-  useEffect(()=> {
+  useEffect(() => {
     console.log(searchResults, 'search results')
-    console.log(markers, 'markers')
+    console.log(markers, 'markers, in useEffect')
 
   }, [searchResults, markers])
 
@@ -83,6 +85,7 @@ function MapsContainer(props) {
 
     setState(prev => ({
       ...prev,
+      markers: [{ lat: lat, lng: lng, name: 'Current Location' }],
       mapsLoaded: true,
       map,
       mapsApi,
@@ -92,7 +95,7 @@ function MapsContainer(props) {
       geoCoderService: new mapsApi.Geocoder(),
       directionService: new mapsApi.DirectionsService(),
     }));
-    addMarker(lat, lng, 'Current Location')
+    // addMarker(lat, lng, 'Current Location')
   });
 
   // Update name for constraint with index === key
@@ -109,31 +112,83 @@ function MapsContainer(props) {
   const updateConstraintTime = ((key, value, event) => {
     const prevConstraints = state.constraints;
     const constraints = Object.assign([], prevConstraints);
-    
+
     constraints[key].time = value;
     setState(prev => ({ ...prev, constraints }));
   });
-  
-  // find Lat and Long of address and add Marker
-  const geoCode = (value) => {
-    
-    geoCoderService.geocode({ address: value.address }, ((response) => {
-      
-      const { location } = response ? response[0].geometry : {location: false}
-      location && addMarker(location.lat(), location.lng(), value.name);
-    }))
+
+
+  // geocode 2
+  async function geoCode(array){
+    const holderArray = markers
+    // console.log(holderArray, 'holderArray1')
+
+    await array.forEach(store => {
+      geoCodeAsync(store).then(res => res && holderArray.push(res))
+    })
+    // console.log(holderArray, 'holderArray2')
+    // return holderArray
+  }
+
+
+
+  async function geoCodeAsync(store) {
+    let promise = new Promise((resolve, reject) => {
+      geoCoderService.geocode({ address: store.address }, ((response) => {
+        const { location } = response ? response[0].geometry : { location: false }
+
+        let coord = location && { lat: location.lat(), lng: location.lng(), name: store.name }
+
+        resolve(coord)
+      }))
+    })
+
+    let result = await promise
+    if (result != undefined){
+      setState(prev => ({...prev, ...{markers: [...prev.markers, result]}}))
+      return result
+    }
+  }
+
+  const renderMarkers = () => {
+    let newArr = markers 
+    let finalObj = {}
+    let finalArr = []
+    console.log(newArr, 'newArr', markers, 'markers state')
+
+    console.log(finalObj, 'finalObj BEFORE filter', finalArr, 'finalArr')
+
+    newArr = newArr.forEach(store => finalObj[store.name] = store)
+    finalArr = Object.values(finalObj)
+
+    console.log(finalObj, 'finalObj after filter', finalArr, 'finalArr')
+    return finalArr.map((marker, key) => {
+      const { name, lat, lng } = marker 
+      return ( <MapMarker key={key} name={name} lat={lat} lng={lng} />)
+    })
   }
   
 
+
+  Array.prototype.unique = function () {
+    var arr = [];
+    for (var i = 0; i < this.length; i++) {
+      if (!arr.includes(this[i])) {
+        arr.push(this[i]);
+      }
+    }
+    return arr;
+  }
+
   // Adds a Marker to the GoogleMaps component
-  const addMarker = ((lat, lng, name) => {
+  const addMarker00 = ((lat, lng, name) => {
     const prevMarkers = state.markers;
     const markers = Object.assign([], prevMarkers);
     console.log("prv markers", prevMarkers, "markers", markers)
     // If name already exists in marker list just replace lat & lng.
     let newMarker = true;
     for (let i = 0; i < markers.length; i++) {
-      debugger
+      // debugger
       if (markers[i].name === name) {
         newMarker = false;
         markers[i].lat = lat;
@@ -143,28 +198,28 @@ function MapsContainer(props) {
       }
     }
     // newMarker = markers.includes({lat: lat, lng: lng, name: name }) ? false : true
-    
+
     // Name does not exist in marker list. Create new marker
     if (newMarker) {
       markers.push({ lat: lat, lng: lng, name: name });
       // setState(prev => ({...prev, markers: [...prev.markers, {lat, lng, name }]}))
       message.success(`Added new "${name}" Marker`);
     }
-    
-    setState(prev => ({ ...prev, ...{markers: markers} }));
+    console.log(markers, 'in addMarker, right before setState')
+    setState(prev => ({ ...prev, ...{ markers: markers } }));
   });
-
 
 
   // With the constraints, find some places serving ice-cream
   const handleSearch = (() => {
     // const { markers, constraints, placesService, directionService, mapsApi } = state;
-    
+
     if (markers.length === 0) {
       message.warn('Add a constraint and try again!');
       return;
     }
     const filteredResults = [];
+    let newMarkers = []
     const marker = markers[0];
     const timeLimit = constraints[0].time;
     const markerLatLng = new mapsApi.LatLng(marker.lat, marker.lng);
@@ -181,7 +236,7 @@ function MapsContainer(props) {
     placesService.textSearch(placesRequest, ((response) => {
       // Only look at the nearest top 5.
       const responseLimit = Math.min(5, response.length);
-      
+
       for (let i = 0; i < responseLimit; i++) {
         const weedShop = response[i];
         const { rating, name } = weedShop;
@@ -190,7 +245,7 @@ function MapsContainer(props) {
         let photoUrl = '';
         let openNow = false;
         if (weedShop.opening_hours) {
-          openNow = weedShop.opening_hours.isOpen() ; // e.g true/false
+          openNow = weedShop.opening_hours.isOpen(); // e.g true/false
         }
         if (weedShop.photos && weedShop.photos.length > 0) {
           photoUrl = weedShop.photos[0].getUrl();
@@ -202,7 +257,7 @@ function MapsContainer(props) {
           destination: address, // Address of ice cream place
           travelMode: 'DRIVING',
         }
-        directionService.route(directionRequest, ((result, status) => {
+        directionService.route(directionRequest, (async (result, status) => {
           if (status !== 'OK') { return }
           const travellingRoute = result.routes[0].legs[0]; // { duration: { text: 1mins, value: 600 } }
           const travellingTimeInMinutes = travellingRoute.duration.value / 60;
@@ -221,10 +276,21 @@ function MapsContainer(props) {
             });
           }
           // Finally, Add results to state
-          console.log(filteredResults, 'filtered results')
-          filteredResults.forEach(res => geoCode(res))
-          setState(prev => ({...prev, ...{searchResults: filteredResults} }))
+          // console.log(filteredResults, 'filtered results')
+          // console.log(newMarkers, 'newMarkers')
+          // filteredResults.forEach(res => geoCode(res))
+          // const newMarkers = geoCode(filteredResults)
+          newMarkers = await geoCode(filteredResults)
+          console.log(newMarkers, 'new Markers line 280')
+          // REMOVE Duplicates
+          // setTheMarkersState(newMarkers)
+          if(i === responseLimit - 1){
+            setState(prev => ({ ...prev, ...{ searchResults: filteredResults, markers: newMarkers, renderMarkersNow: true } }))
+          } else {
+            setState(prev => ({ ...prev, ...{ searchResults: filteredResults, markers: newMarkers, renderMarkersNow: false } }))
+          }
           
+
         }));
       }
     }));
@@ -250,7 +316,7 @@ function MapsContainer(props) {
                   <div className={classes.nothing}>
                   </div>
                   < section className="" >
-                  <span className="">Minutes away by car</span>
+                    <span className="">Minutes away by car</span>
                     <div className="">
                       <Slider className="" value={time} min={0} max={60} onChange={(event, value) => updateConstraintTime(key, value, event)} valueLabelDisplay="auto" />
                     </div>
@@ -277,12 +343,12 @@ function MapsContainer(props) {
           onGoogleApiLoaded={({ map, maps }) => apiHasLoaded(map, maps)} // "maps" is the mapApi. Bad naming but that's their library.
         >
           {/* Pin markers on the Map*/}
-          {markers && markers.map((marker, key) => {
-            const { name, lat, lng } = marker;
-            return (
-              <MapMarker key={key} name={name} lat={lat} lng={lng} />
-            );
-          })}
+          {/* {(markers && renderMarkersNow) && renderMarkers()} */}
+          {markers && renderMarkers()}
+          {/* {markers && markers.map((marker, key) => {
+      const { name, lat, lng } = marker 
+      return ( <MapMarker key={key} name={name} lat={lat} lng={lng} />)
+    })} */}
         </GoogleMapReact>}
       </section>
 
@@ -297,7 +363,7 @@ function MapsContainer(props) {
             {/* "d-flex flex-column justify-content-center" */}
             <div className={classes.nothing}>
               {/* "mb-4 fw-md" */}
-              <h1 className={classes.nothing}>Tadah! Ice-Creams!</h1>
+              <h1 className={classes.nothing}>Tadah! Cannabis Stores!</h1>
               {/* "d-flex flex-wrap" */}
               <div className={classes.nothing}>
                 {searchResults.map((result, key) => (
@@ -313,4 +379,4 @@ function MapsContainer(props) {
 
 }
 
-export default MapsContainer;
+export default MapsContainer
