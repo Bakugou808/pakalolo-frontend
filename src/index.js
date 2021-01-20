6518b4d54b20c5321d75bf352d4e9bb72b0f5e24
Janu <janu.m.sung@gmail.com>
2021-01-19 18:53:35 -0800

    removed redux devtools ext

--- src/index.js
+++ src/index.js
@@ -36,9 +36,9 @@ const theme = createMuiTheme({
 let store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(logger),
+    applyMiddleware(logger)
     // applyMiddleware(logger, thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
