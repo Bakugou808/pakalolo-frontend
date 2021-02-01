e07ab373c35a4639a69b4dff6431fab3f58c89c4
Janu <janu.m.sung@gmail.com>
2021-01-31 16:24:10 -0800

    took out middleware

--- src/index.js
+++ src/index.js
@@ -35,11 +35,10 @@ const theme = createMuiTheme({
 
 let store = createStore(
   rootReducer,
-  compose(
-    // applyMiddleware(logger)
-    applyMiddleware(logger, thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose()
+  // applyMiddleware(logger)
+  // applyMiddleware(logger, thunk),
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 ReactDOM.render(
